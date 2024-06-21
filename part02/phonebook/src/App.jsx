7301e516c5923b6fd6945dff3f73312a4295d6e5
Aaron Kilpatrick <kilpatrick.aaron92@gmail.com>
2024-06-21 16:56:08 +1000

    Complete up to part02/2.7

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function App() {
+  const [person, setPerson] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+  
+  const checkUniqueName = () => {
+    return person.find((person) => person.name === newName);
+  }
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    if (checkUniqueName()) 
+      return alert(`${newName} is already added to phonebook`);
+
+    setPerson(person.concat({ name: newName }));
+    setNewName('');
+  }
+
+  const handleInputChange = (event) => setNewName(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {person.map(entry => <p key={entry.name}>{entry.name}</p>)}
+    </div>
+  );
+}
+
+export default App;
