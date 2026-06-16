15345be024ec0bc52c1df9f91cec69fcfa82f177
aaron <kilpatrick.aaron92@gmail.com>
2026-06-16 16:13:11 +1000

    add / endpoint

--- src/part3/notes/backend/index.js
+++ src/part3/notes/backend/index.js
@@ -34,6 +34,10 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world!</h1>");
+});
+
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
