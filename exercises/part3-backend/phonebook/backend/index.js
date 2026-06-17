226cea3cf84ceed9c0636d85ba5b293a87ae1f31
aaron <kilpatrick.aaron92@gmail.com>
2026-06-17 12:23:50 +1000

    Exercise 3.10 - update port env

--- exercises/part3-backend/phonebook/backend/index.js
+++ exercises/part3-backend/phonebook/backend/index.js
@@ -110,7 +110,7 @@ function nameExists(name) {
     (person) => person.name.toLowerCase() === name.toLowerCase(),
   );
 }
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
